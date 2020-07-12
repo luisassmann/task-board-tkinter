b11cb706d2ef22010703ba4f564f8118e142cb30
luisassmann <luisassmann@acer.localdomain>
2020-07-11 22:38:31 -0300

    refactor code

---
+++ cache.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter import tix
+from tkinter import ttk
+import sqlite3
+
+class task():
+    def variaveis_tarefas(self):
+        self.tarefa_TODO = {
+            "codigo": "",
+            "titulo": "",
+            "descricao": "",
+            "prazo": ""
+        }
+
+        self.tarefa_DO = {
+            "codigo": "",
+            "titulo": "",
+            "descricao": "",
+            "prazo": ""
+        }
+
+        self.tarefa_DONE = {
+            "codigo": "",
+            "titulo": "",
+            "descricao": "",
+            "prazo": ""
+        }
