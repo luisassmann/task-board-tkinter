e8ce3b114c09a8d58fdf4ca3d166939caf855a48
luisassmann <luisassmann@users.noreply.github.com>
2020-07-25 17:25:18 -0300

    refresh app

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from src.cache import *
 
 root = Tk()
 
-# ------ FRONT - END ---------
+
 class Aplication(funcs):
     def __init__(self):
         self.root = root
@@ -254,45 +254,45 @@ class Aplication(funcs):
     def Tarefa_a_fazer(self):
         self.padrao_tarefas()
         self.show_in_frame_1()
-        self.tituloF1 = Entry(self.frame1, text=self.tarefa_TODO["titulo"], bg='#e5e5e5',
+        self.tituloF1 = Entry(self.frame1, bg='#e5e5e5',
                               fg='#0c0c0c', font=('Roboto', 14, 'bold'),
                               justify='center', bd=0)
         self.tituloF1.place(relx=0.1, rely=0.01, relwidth=0.8, relheight=0.25)
 
-        self.descricF1 = Entry(self.frame1, text=self.tarefa_TODO["descricao"], bg='#e5e5e5',
-                               fg='#0c0c0c', font=('Roboto', 13), bd=0)
+        self.descricF1 = Text(self.frame1, bg='#e5e5e5', borderwidth=0,
+                               fg='#0c0c0c', font=('Roboto', 13))
         self.descricF1.place(relx=0.02, rely=0.35, relwidth=0.5, relheight=0.5)
 
-        self.prazoF1 = Entry(self.frame1, text=self.tarefa_TODO["prazo"], bg='#e5e5e5',
+        self.prazoF1 = Entry(self.frame1, bg='#e5e5e5',
                              fg='#0c0c0c', font=('Roboto', 13), justify='center', bd=0)
         self.prazoF1.place(relx=0.6, rely=0.5, relwidth=0.3, relheight=0.2)
 
     def Tarefa_fazendo(self):
         self.variaveis_tarefas()
-        self.tituloF2 = Entry(self.frame2, text=self.tarefa_DO["titulo"], bg='#e5e5e5',
+        self.tituloF2 = Entry(self.frame2, bg='#e5e5e5',
                               fg='#0c0c0c', font=('Roboto', 14, 'bold'),
                               justify='center', bd=0)
         self.tituloF2.place(relx=0.1, rely=0.01, relwidth=0.8, relheight=0.25)
 
-        self.descricF2 = Entry(self.frame2, text=self.tarefa_DO["descricao"], bg='#e5e5e5',
-                               fg='#0c0c0c', font=('Roboto', 13), bd=0)
+        self.descricF2 = Text(self.frame2, bg='#e5e5e5', borderwidth=0,
+                               fg='#0c0c0c', font=('Roboto', 13))
         self.descricF2.place(relx=0.02, rely=0.35, relwidth=0.5, relheight=0.5)
 
-        self.prazoF2 = Entry(self.frame2, text=self.tarefa_DO["prazo"], bg='#e5e5e5',
+        self.prazoF2 = Entry(self.frame2, bg='#e5e5e5',
                              fg='#0c0c0c', font=('Roboto', 13), justify='center', bd=0)
         self.prazoF2.place(relx=0.6, rely=0.5, relwidth=0.3, relheight=0.2)
 
     def Tarefa_feita(self):
-        self.tituloF3 = Entry(self.frame3, text='', bg='#e5e5e5',
+        self.tituloF3 = Entry(self.frame3, bg='#e5e5e5',
                               fg='#0c0c0c', font=('Roboto', 14, 'bold'),
                               justify='center', bd=0)
         self.tituloF3.place(relx=0.1, rely=0.01, relwidth=0.8, relheight=0.25)
 
-        self.descricF3 = Entry(self.frame3, text='', bg='#e5e5e5',
-                               fg='#0c0c0c', font=('Roboto', 13), bd=0)
+        self.descricF3 = Text(self.frame3, bg='#e5e5e5', borderwidth=0,
+                               fg='#0c0c0c', font=('Roboto', 13))
         self.descricF3.place(relx=0.02, rely=0.35, relwidth=0.5, relheight=0.5)
 
-        self.prazoF3 = Entry(self.frame3, text='', bg='#e5e5e5',
+        self.prazoF3 = Entry(self.frame3, bg='#e5e5e5',
                              fg='#0c0c0c', font=('Roboto', 13), justify='center', bd=0)
         self.prazoF3.place(relx=0.6, rely=0.5, relwidth=0.3, relheight=0.2)
 
